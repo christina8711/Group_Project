8f39f7974e24cd7c6818027bfeb82389e480695f
NotBonus <115891081+NotBonus@users.noreply.github.com>
2022-11-10 02:00:31 -0500

    Test

--- 2-D Platformer Draft/Assets/Scripts/CoinScript.cs
+++ 2-D Platformer Draft/Assets/Scripts/CoinScript.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class CoinScript : MonoBehaviour
 {
+    public AudioClip coinSound;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         ScoreTextScript.coinAmount += 100;
+        AudioSource.PlayClipAtPoint(coinSound, transform.position);
         Destroy(gameObject);
     }
 }
